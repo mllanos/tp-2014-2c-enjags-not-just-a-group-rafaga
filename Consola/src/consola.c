25e2bc48b50facfd6b23e9416026629e3adc7798
fedegonzalez <fedegonzalez_92@hotmail.com>
2014-09-10 17:30:19 -0300

    avance con consola, archivo de configuracion levantado

--- Consola/src/consola.c
+++ Consola/src/consola.c
@@ -1,5 +1,17 @@
-#include <stdlib.h>
+#include "consola.h"
 
-int main (){
+char *direccionIP;
+int puerto;
+
+
+int main (int argc, char *argv[]){
+
+	//Levantar archivo de configuracion
+	t_config* config;
+	config=config_create(PATH_ARCHIVO_CONF);
+	puerto=config_get_int_value(config,"PUERTO_KERNEL");
+	direccionIP=config_get_string_value(config,"IP_KERNEL");
+	//Fin levantar archivo de configuracion
+
 	return 0;
 }
--- Consola/src/consola.h
+++ Consola/src/consola.h
@@ -6,8 +6,23 @@
  */
 
 #ifndef CONSOLA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <commons/config.h>
+#include <commons/log.h>
+
+#define PATH_ARCHIVO_CONF "Config/consola.conf"
+
+
+
+
+
+
+
+
+
 #define CONSOLA_H_
-
-
 
 #endif /* CONSOLA_H_ */
