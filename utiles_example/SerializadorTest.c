d3cbef6e8851b9456c23fa747a412dd4abedd147
volcakeys <suncloudm2@gmail.com>
2014-10-05 19:17:15 -0300

    agrego funcion maneja direcciones

--- utiles_example/SerializadorTest.c
+++ utiles_example/SerializadorTest.c
@@ -40,3 +40,15 @@ int main(void) {
 	return 0;
 
 }
+
+void more_stuff(uint32_t value) {             // Example value: 0x01020304
+    uint32_t byte1 = (value >> 24);           // 0x01020304 >> 24 is 0x01 so
+                                              // no masking is necessary
+    uint32_t byte2 = (value >> 16) & 0xff;    // 0x01020304 >> 16 is 0x0102 so
+                                              // we must mask to get 0x02
+    uint32_t byte3 = (value >> 8)  & 0xff;    // 0x01020304 >> 8 is 0x010203 so
+                                              // we must mask to get 0x03
+    uint32_t byte4 = value & 0xff;            // here we only mask, no shifting
+                                              // is necessary
+    ...
+}
